14168feb748fa9f056882cb5508a4b7cd95e4bfa
Arkyadeep pal <arkyadeep.pal2017@gmail.com>
2026-01-26 23:32:19 +0530

    Add main page entry

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Home from "@/app/Home/Home"
+import SmoothScrollHero from "@/components/SmoothScrollHero";
+
+export default function Page() {
+  return (
+    <div>
+      {/* <Home /> */}
+      <SmoothScrollHero />
+    </div>
+  );
+}
